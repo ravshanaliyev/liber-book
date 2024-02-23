ea7f2d0051f141c7d3ab3ce3a6666e817727dc7e
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-02-23 18:31:59 +0500

    settings page

--- app/profile/sozlamalar/page.tsx
+++ app/profile/sozlamalar/page.tsx
@@ -1,8 +1,17 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const Sozlamalar = () => {
     return (
-        <div>Sozlamalar</div>
+        <div className='border rounded-xl shadow-lg p-6'>
+            <h1 className='text-3xl'>Sozlamalar</h1>
+            <p className='my-2'>Sizning sozlamalaringiz</p>
+            <Input className='w-[300px] my-3' type="text" placeholder="username" />
+            <Input className='w-[300px] my-3' type="email" placeholder="email" />
+            <Input className='w-[300px] my-3' type="password" placeholder="password" />
+            <Button className='w-[300px] my-3'>Saqlash</Button>
+        </div>
     )
 }
 
